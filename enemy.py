1420e68c66b81b568a0dde88c396b5ba039f595d
Adam Varghese <81534366+a8varghe@users.noreply.github.com>
2022-06-09 08:53:24 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -22,18 +22,18 @@ class QixEnemy(object):
 
     def move(self, dt, game_board, game_states, config):
         
-        # lc = 0
-        # rc = 0
-        # tc = 0
-        # bc = 0
+        lc = 0;
+        rc = 0;
+        tc = 0;
+        bc = 0;
         l_dir, r_dir, t_dir, b_dir = self.wallCollision(game_board.game_matrix)
 
-        playerCollision = foundQIX(config['player'].player_coords['x'], config['player'].player_coords['y'], self.x, self.y, self.radius, 15)
+        playerCollision = foundQIX(config['player'].player_coords['x'], config['player'].player_coords['y'], self.x, self.y, self.radius, 15);
         potential_wall_collision = False
         for wall in game_board.potentialWalls:
             if foundQIX(wall[0], wall[1], self.x, self.y, self.radius, 15):
                 potential_wall_collision = True
-                break
+                break;
 
 
         if playerCollision or potential_wall_collision:
@@ -44,66 +44,68 @@ class QixEnemy(object):
             game_states['insideGame']['tryingToCapture'] = False
 
         elif (l_dir or r_dir or t_dir or b_dir):
+            print('hitting wall');
             # time.sleep(1)
             if l_dir:
                 # print('here 11111111111111111:', random.uniform(5, 10))
-                #lc = lc + 1
+                lc = lc + 1;
                 self.dx = -abs(self.dx)
                 tempDx = self.dx - random.uniform(5, 10)
                 self.x += tempDx
-                # self.y = ceil(self.y)
+                # self.y = ceil(self.y);
                 self.y += self.dy * dt
 
                 # if (lc >= 10):
                 #     self.x += self.dx - random.uniform(0.2, 0.5)
-                #     lc = 0
+                #     lc = 0;
 
-            elif r_dir:
-                #rc = rc + 1
+            if r_dir:
+                rc = rc + 1;
                 self.dx = abs(self.dx)
                 tempDx = self.dx + random.uniform(5, 10)
                 self.x += tempDx
-                # self.y = floor(self.y)
+                # self.y = floor(self.y);
                 self.y += self.dy * dt
 
                 # special edge cases
                 # if (rc >= 10):
-                #     print('here 2', l_dir, r_dir, t_dir, b_dir)
+                #     print('here 2', l_dir, r_dir, t_dir, b_dir);
                 #     self.x += self.dx + random.uniform(0.2, 0.5)
-                #     rc = 0
+                #     rc = 0;
                 # elif (rc >= 3 and bc >= 3):
                 #     self.x += 10
 
             
             if t_dir:
-                #tc = tc + 1
+                tc = tc + 1;
                 self.dy = abs(self.dy)
                 tempDy = self.dy + random.uniform(5, 10)
                 self.y += tempDy
-                # self.x = floor(self.x)
+                # self.x = floor(self.x);
                 self.x += self.dx * dt
 
                 # if (tc >= 10):
                 #     self.y += self.dy + random.uniform(0.5, 0.7)
-                #     tc = 0
+                #     tc = 0;
 
-            elif b_dir:
-                #bc = bc + 1
+            if b_dir:
+                bc = bc + 1;
                 self.dy = - abs(self.dy)
                 tempDy = self.dy - random.uniform(5, 10)
                 self.y += tempDy
-                # self.x = ceil(self.x)
+                # self.x = ceil(self.x);
                 self.x += self.dx * dt
 
                 # if (bc >= 10):
                 #     self.y += self.dy - random.uniform(0.5, 0.7)
-                #     bc = 0
+                #     bc = 0;
 
         else:
-            # lc = 0
-            # rc = 0
-            # tc = 0
-            # bc = 0
+            lc = 0;
+            rc = 0;
+            tc = 0;
+            bc = 0;
+            # print('normal move')
             self.x += self.dx * dt
             self.y += self.dy * dt
 
@@ -111,21 +113,20 @@ class QixEnemy(object):
 
         ############# game area bounds checks, only needed for precaution
         # check if out of bounds in x axis
-        # if self.x <= 0:
-        #     self.dx = abs(self.dx)
-        #     self.x += self.dx
-        # elif self.x >= config['width']:
-        #     self.dx = -abs(self.dx)
-        #     self.x += self.dx
+        if self.x <= 0:
+            self.dx = abs(self.dx)
+            self.x += self.dx
+        elif self.x >= config['width']:
+            self.dx = -abs(self.dx)
+            self.x += self.dx
         
-        # # check if out of bounds in y axis
-        # if self.y <= 0:
-        #     self.dy = abs(self.dy)
-        #     self.y += self.dy
-        # elif self.y >= config['height']:
-        #     print(config['height'])
-        #     self.dy = -abs(self.dy)
-        #     self.y += self.dy
+        # check if out of bounds in y axis
+        if self.y <= 0:
+            self.dy = abs(self.dy)
+            self.y += self.dy
+        elif self.y >= config['height']:
+            self.dy = -abs(self.dy)
+            self.y += self.dy
 
     
     def wallCollision(self, gameMatrix):
@@ -138,12 +139,15 @@ class QixEnemy(object):
                 for col in range(0, len(gameMatrix[row])):
                     if gameMatrix[row][col]['w'] == True:
                         l_dir, r_dir, t_dir, b_dir = False, False, False, False
-                        intersect = self.circleIntersection(self.x, self.y, gameMatrix[row][col]['x'] + 25, gameMatrix[row][col]['y'] + 25, self.radius, 25)
+                        intersect = self.circleIntersection(self.x, self.y, gameMatrix[row][col]['x'] + 25, gameMatrix[row][col]['y'] + 25, self.radius, 25);
                         if intersect:
+                            print('intersect')
+
                             if abs(self.y - gameMatrix[row][col]['y']) <= abs(self.x - gameMatrix[row][col]['x']):
                                 if gameMatrix[row][col]['y'] + 25 >= self.y:
                                     b_dir = True
                                     # check left
+                                    print('ball hit bottom side of wall')
                                     if self.matrixCoordIsValid(row + 1, col - 1, gameMatrix):
                                         if gameMatrix[row + 1][col - 1]['x'] + 25 >= self.x:
                                             l_dir = True
@@ -152,10 +156,11 @@ class QixEnemy(object):
                                         if gameMatrix[row + 1][col + 1]['x'] + 25 <= self.x:
                                             r_dir = True
                                     break
-                                # both intersect
+                                # bot intersect
                                 elif gameMatrix[row][col]['y'] + 25 <= self.y:
                                     t_dir = True
                                     # check left
+                                    print('ball hit top side of wall')
                                     if self.matrixCoordIsValid(row - 1, col - 1, gameMatrix):
                                         if gameMatrix[row - 1][col - 1]['x'] + 25 >= self.x:
                                             l_dir = True
@@ -168,6 +173,7 @@ class QixEnemy(object):
                                 # left intersect
                                 if gameMatrix[row][col]['x'] + 25 >= self.x:
                                     l_dir = True
+                                    print('ball hit left side of wall')
                                     # check top
                                     if self.matrixCoordIsValid(row - 1, col + 1, gameMatrix):
                                         if gameMatrix[row - 1][col + 1]['y'] + 25 >= self.y:
@@ -176,10 +182,11 @@ class QixEnemy(object):
                                     elif self.matrixCoordIsValid(row + 1, col + 1, gameMatrix):
                                         if gameMatrix[row + 1][col + 1]['y'] + 25 <= self.y:
                                             b_dir = True
-                                    break
+                                    break;
                                 # right intersect
                                 elif gameMatrix[row][col]['x'] + 25 <= self.x:
                                     r_dir = True
+                                    print('ball hit right side of wall')
                                     # check top
                                     if self.matrixCoordIsValid(row - 1, col - 1, gameMatrix):
                                         if gameMatrix[row - 1][col - 1]['y'] + 25 >= self.y:
@@ -188,7 +195,7 @@ class QixEnemy(object):
                                     elif self.matrixCoordIsValid(row + 1, col - 1, gameMatrix):
                                         if gameMatrix[row + 1][col - 1]['y'] + 25 <= self.y:
                                             b_dir = True
-                                    break
+                                    break;
 
 
 
